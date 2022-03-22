3947788af75f5b53e4165aa1b6e83c83415f4a0c
Orit Moskovich <orit.mosko@gmail.com>
2022-03-22 12:39:00 +0200

    Fix scroll issue + disabling text selection events + add background and some ui improvements

--- sketch.js
+++ sketch.js
@@ -10,28 +10,34 @@ let canvas;
 let chosenImage;
 let imageGraphics;
 
-let scale = 0.7;
+let scaleImg;
 let displayedImageSize;
 
 function setup() {
+  // Disable elemnts selection.
+  disableElementsSelection();
+
   // Keep image names.
   for (let i = 1; i <= numOfImages; i++) {
     imageSources.push(`assets/${i}.webp`);
   }
 
   // Setup canvas over image.
-  let scale = 1;
-  if (displayWidth <= imageSize) {
-    scale = 0.6;
+  let scaleImg = 1;
+  let heightLeft = displayHeight - document.getElementById("revealImage").offsetTop - document.getElementById("revealImage").offsetHeight;
+  if (displayWidth <= imageSize || heightLeft <= imageSize) {
+    scaleImg = 0.6;
   }
   let canvasOffsetTop = document.getElementById('img').offsetTop / 1.2;
 
-  displayedImageSize = Math.min(displayWidth, imageSize) * scale;
+  displayedImageSize = Math.min(Math.min(displayWidth, heightLeft), imageSize) * scaleImg;
+  console.log("orittt");
+  console.log(displayedImageSize, displayHeight, scaleImg);
   document.getElementById("img").height = displayedImageSize;
   document.getElementById("img").width = displayedImageSize;
 
   pixelDensity(1);
-  canvas = createCanvas(displayedImageSize * 1.3, displayedImageSize * 1.2);
+  canvas = createCanvas(displayedImageSize * 1.3, displayedImageSize + 20);
   canvas.position(0, canvasOffsetTop);
   canvas.center('horizontal');
 
@@ -52,12 +58,46 @@ function draw() {
   }
 }
 
+function disableElementsSelection() {
+  let unFocus = function () {
+    if (document.selection) {
+        document.selection.empty()
+    } else {
+        window.getSelection().removeAllRanges()
+    }
+  }
+  document.getElementById('revealImage').onmousemove = function () {
+      unFocus()
+  }
+  document.getElementById('revealImage').onmouseup = function () {
+      unFocus()
+  }
+  document.getElementById('renderNew').onmousemove = function () {
+      unFocus()
+  }
+  document.getElementById('renderNew').onmouseup = function () {
+      unFocus()
+  }
+  document.getElementById('header').onmousemove = function () {
+      unFocus()
+  }
+  document.getElementById('header').onmouseup = function () {
+      unFocus()
+  }
+  document.getElementById('imgDiv').onmousemove = function () {
+      unFocus()
+  }
+  document.getElementById('imgDiv').onmouseup = function () {
+      unFocus()
+  }
+}
+
 function renderNewImage() {
   // Clear canvas.
   push();
   background(255);
-  strokeWeight(6);
-  stroke(200);
+  strokeWeight(12);
+  stroke(100);
   rect(0, 0, displayedImageSize * 1.299, displayedImageSize * 1.199 ,10);
   pop();
   // Generate new image.
